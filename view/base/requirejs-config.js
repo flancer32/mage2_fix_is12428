e263b2004adb8b3dda6844ea96301d0fb8caada0
Alex Gusev <alex@flancer64.com>
2018-04-04 12:58:46 +0300

    Issue 12428

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -1,6 +1,7 @@
 /* see http://devdocs.magento.com/guides/v2.0/javascript-dev-guide/javascript/requirejs_concept.html */
 /* add custom error handler to 'require' object */
 var config = {};
+/* debugger; */
 require.onError = function issue12428ErrorHandler(e) {
     console.error("RequireJS Error (issue 12428)", e);
 }
